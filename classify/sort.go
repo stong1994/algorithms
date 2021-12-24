9bf0d1f47339a2ae7975f812b202b77427941480
stong <498327580@qq.com>
2021-12-24 15:16:49 +0800

    排序

--- classify/sort.go
+++ classify/sort.go
@@ -1,5 +1,9 @@
 package classify
 
+import (
+	"math/rand"
+)
+
 // 排序应用（源于《算法》第2.5章节-应用）
 // 1. 找出重复元素——对数组排序，然后遍历一遍即可找到重复元素。（在不考虑内存的情况下，使用hash更快）
 // 2. 排名
@@ -25,17 +29,212 @@ func topN(list []int, k int) int {
 }
 
 func quickPartition(list []int, left, right int) int {
-	i, j, p := left, right+1, left
+	p := rand.Intn(right-left+1) + left // 随机选择一个元素
+	pivot := list[p]
+	list[p], list[left] = list[left], list[p] // 将随机选择的元素放到第一个
+	i, j := left, right+1
 	for {
-		for i++; list[p] <= list[i] && i < right; i++ {
+		for i++; i < right && list[i] >= pivot; i++ {
 		}
-		for j--; list[p] >= list[j] && j > left; j-- {
+		for j--; j > left && list[j] <= pivot; j-- {
 		}
 		if i >= j {
 			break
 		}
 		list[i], list[j] = list[j], list[i]
 	}
-	list[p], list[j] = list[j], list[p]
+	list[left], list[j] = list[j], list[left]
 	return j
+}
+
+// 以下源自：https://github.com/CyC2018/CS-Notes/blob/master/notes/Leetcode%20%E9%A2%98%E8%A7%A3%20-%20%E6%8E%92%E5%BA%8F.md
+// 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+// https://leetcode-cn.com/problems/kth-largest-element-in-an-array/description/
+func findKthLargest(nums []int, k int) int {
+	k-- // 第k个数的索引为k-1
+	left, right := 0, len(nums)-1
+	for left < right {
+		j := quickPartition(nums, left, right)
+		if j == k {
+			return nums[j]
+		}
+		if j > k {
+			right = j - 1
+		} else {
+			left = j + 1
+		}
+	}
+	return nums[k]
+}
+
+// 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+// https://leetcode-cn.com/problems/top-k-frequent-elements/description/
+// 转换为TopN问题：构造结构体{num, count}，然后将nums数组转换为新的结构体数组，然后进行快速排序找到partition
+func topKFrequent_0(nums []int, k int) []int {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	type node struct {
+		num   int
+		count int
+	}
+
+	nodeQuickPartition := func(list []node, left, right int) int {
+		p := rand.Intn(right-left+1) + left // 随机选择一个元素
+		pivot := list[p].count
+		list[p], list[left] = list[left], list[p] // 将随机选择的元素放到第一个
+		i, j := left, right+1
+		for {
+			for i++; i < right && list[i].count >= pivot; i++ {
+			}
+			for j--; j > left && list[j].count <= pivot; j-- {
+			}
+			if i >= j {
+				break
+			}
+			list[i], list[j] = list[j], list[i]
+		}
+		list[left], list[j] = list[j], list[left]
+		return j
+	}
+
+	m := make(map[int]int)
+	arr := make([]node, 0, k)
+	for _, v := range nums {
+		if r, ok := m[v]; ok {
+			m[v] = r + 1
+			continue
+		}
+		m[v] = 1
+	}
+
+	if len(m) <= k {
+		return mapKey2Arr(m)
+	}
+
+	for num, count := range m {
+		arr = append(arr, node{
+			num:   num,
+			count: count,
+		})
+	}
+
+	getMaxKNode := func() []node {
+		k-- // 第k个数的索引为k-1
+		left, right := 0, len(arr)-1
+		for left < right {
+			j := nodeQuickPartition(arr, left, right)
+			if j == k {
+				return arr[:j+1]
+			}
+			if j > k {
+				right = j - 1
+			} else {
+				left = j + 1
+			}
+		}
+		return arr[:k+1]
+	}
+
+	ns := getMaxKNode()
+	result := make([]int, len(ns))
+	for i, v := range ns {
+		result[i] = v.num
+	}
+	return result
+}
+
+func mapKey2Arr(m map[int]int) []int {
+	result := make([]int, 0, len(m))
+	for k := range m {
+		result = append(result, k)
+	}
+	return result
+}
+
+// 解法2：桶排序
+func topKFrequent(nums []int, k int) []int {
+	arr := []map[int]struct{}{0: {}, 1: {}} // 数量-值
+	m := make(map[int]int)
+	for _, v := range nums {
+		if n, ok := m[v]; ok {
+			m[v] = n + 1
+			delete(arr[n], v)
+			if len(arr) == n+1 {
+				arr = append(arr, map[int]struct{}{v: {}})
+			} else if arr[n+1] == nil {
+				arr[n+1] = map[int]struct{}{v: {}}
+			} else {
+				arr[n+1][v] = struct{}{}
+			}
+			continue
+		}
+		m[v] = 1
+		arr[1][v] = struct{}{}
+	}
+	var result []int
+	for i := len(arr) - 1; i >= 1; i-- {
+		for n := range arr[i] {
+			if len(result) < k {
+				result = append(result, n)
+			}
+		}
+	}
+	return result
+}
+
+// 给定一个字符串，请将字符串里的字符按照出现的频率降序排列。
+// https://leetcode-cn.com/problems/sort-characters-by-frequency/description/
+// 基于桶排序
+func frequencySort(s string) string {
+	if s == "" {
+		return ""
+	}
+	arr := []map[int32]struct{}{0: {}, 1: {}} // 数量-值
+	m := make(map[int32]int)
+	for _, v := range s {
+		if n, ok := m[v]; ok {
+			m[v] = n + 1
+			delete(arr[n], v)
+			if len(arr) == n+1 {
+				arr = append(arr, map[int32]struct{}{v: {}})
+			} else if arr[n+1] == nil {
+				arr[n+1] = map[int32]struct{}{v: {}}
+			} else {
+				arr[n+1][v] = struct{}{}
+			}
+			continue
+		}
+		m[v] = 1
+		arr[1][v] = struct{}{}
+	}
+	var result []byte
+	for i := len(arr) - 1; i >= 1; i-- {
+		for n := range arr[i] {
+			for j := 0; j < i; j++ {
+				result = append(result, byte(n))
+			}
+		}
+	}
+	return string(result)
+}
+
+// 荷兰国旗问题
+// 给定一个包含红色、白色和蓝色，一共n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+// 此题中，我们使用整数 0、1 和 2 分别表示红色、白色和蓝色。
+// https://leetcode-cn.com/problems/sort-colors/description/
+// 桶排序
+func sortColors(nums []int) {
+	arr := []int{0, 0, 0}
+	for _, v := range nums {
+		arr[v]++
+	}
+	i := 0
+	for n, v := range arr {
+		for j := 0; j < v; j++ {
+			nums[i] = n
+			i++
+		}
+	}
 }
--- classify/sort_test.go
+++ classify/sort_test.go
@@ -1,10 +1,11 @@
 package classify
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_topN(t *testing.T) {
-	var sortList = []int{2, 4, 1, 2, 9, 11, 5, 6, 3, 1, 0, 10, 8, -1}
-
 	type args struct {
 		list []int
 		k    int
@@ -14,12 +15,176 @@ func Test_topN(t *testing.T) {
 		args args
 		want int
 	}{
-		{name: "topN", args: args{sortList, 5}, want: 6},
+		{
+			name: "topN",
+			args: args{
+				[]int{2, 4, 1, 2, 9, 11, 5, 6, 3, 1, 0, 10, 8, -1},
+				5,
+			},
+			want: 6,
+		},
+		{
+			name: "topN2",
+			args: args{
+				[]int{3, 2, 1, 5, 6, 4},
+				2,
+			},
+			want: 5,
+		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := topN(tt.args.list, tt.args.k); got != tt.want {
 				t.Errorf("topN() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_topKFrequent(t *testing.T) {
+	type args struct {
+		nums []int
+		k    int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		check func(got []int) bool
+	}{
+		{
+			name: "empty",
+			args: args{
+				nums: nil,
+				k:    1,
+			},
+			check: func(got []int) bool {
+				return got == nil
+			},
+		},
+		{
+			name: "k > len(list)",
+			args: args{
+				nums: []int{1, 2, 3},
+				k:    4,
+			},
+			check: func(got []int) bool {
+				if len(got) != 3 {
+					return false
+				}
+				return got[0]*got[1]*got[2] == 6
+			},
+		},
+		{
+			name: "k == len(list)",
+			args: args{
+				nums: []int{1, 2, 3},
+				k:    3,
+			},
+			check: func(got []int) bool {
+				if len(got) != 3 {
+					return false
+				}
+				return got[0]*got[1]*got[2] == 6
+			},
+		},
+		{
+			name: "normal",
+			args: args{
+				nums: []int{1, 2, 2, 3, 3},
+				k:    2,
+			},
+			check: func(got []int) bool {
+				if len(got) != 2 {
+					return false
+				}
+				return got[0]*got[1] == 6
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := topKFrequent(tt.args.nums, tt.args.k); !tt.check(got) {
+				t.Errorf("failed: topKFrequent() = %v", got)
+			}
+		})
+	}
+}
+
+func Test_frequencySort(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		check func(got string) bool
+	}{
+		{
+			name: "empty",
+			args: args{s: ""},
+			check: func(got string) bool {
+				return got == ""
+			},
+		},
+		{
+			name: "simple",
+			args: args{s: "aab"},
+			check: func(got string) bool {
+				return got == "aab"
+			},
+		},
+		{
+			name: "cccaaa",
+			args: args{s: "cccaaa"},
+			check: func(got string) bool {
+				return got == "cccaaa" || got == "aaaccc"
+			},
+		},
+		{
+			name: "tree",
+			args: args{s: "tree"},
+			check: func(got string) bool {
+				return got == "eert" || got == "eetr"
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := frequencySort(tt.args.s); !tt.check(got) {
+				t.Errorf("failed: frequencySort() = %v", got)
+			}
+		})
+	}
+}
+
+func Test_sortColors(t *testing.T) {
+	tests := []struct {
+		name  string
+		nums  []int
+		check func(afterNums []int) bool
+	}{
+		{
+			name: "empty",
+			nums: []int{},
+			check: func(afterNums []int) bool {
+				return len(afterNums) == 0
+			},
+		},
+		{
+			name: "2,0,2,1,1,0",
+			nums: []int{2, 0, 2, 1, 1, 0},
+			check: func(afterNums []int) bool {
+				dst := []int{0, 0, 1, 1, 2, 2}
+				return reflect.DeepEqual(afterNums, dst)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sortColors(tt.nums)
+			if !tt.check(tt.nums) {
+				t.Errorf("failed: name = %s, nums = %v", tt.name, tt.nums)
 			}
 		})
 	}
