6864effaea1fac8a477e588bb925baed7d9aec19
stong <498327580@qq.com>
2022-02-07 22:35:28 +0800

    tree-递归题

--- classify/tree/recursion.go
+++ classify/tree/recursion.go
@@ -282,3 +282,206 @@ func tree2list(tree *TreeNode, list []int) []int {
 	}
 	return list
 }
+
+// 对称二叉树
+// 给你一个二叉树的根节点 root ， 检查它是否轴对称。
+//提示：
+//树中节点数目在范围 [1, 1000] 内
+//-100 <= Node.val <= 100
+//进阶：你可以运用递归和迭代两种方法解决这个问题吗？
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/symmetric-tree
+func isSymmetric(root *TreeNode) bool {
+	// 两颗对称树的特性：根节点值相同，左右节点相反。因此可以直接使用递归
+	var equal func(a, b *TreeNode) bool
+	equal = func(a, b *TreeNode) bool {
+		if a == nil && b == nil {
+			return true
+		}
+		if a == nil || b == nil {
+			return false
+		}
+		if a.Val != b.Val {
+			return false
+		}
+		return equal(a.Left, b.Right) && equal(a.Right, b.Left)
+	}
+	if root == nil {
+		return false
+	}
+	return equal(root.Left, root.Right)
+}
+
+// 二叉树的最小深度
+// 给定一个二叉树，找出其最小深度。
+//最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+//说明：叶子节点是指没有子节点的节点。
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/minimum-depth-of-binary-tree
+func minDepth(root *TreeNode) int {
+	// 最小深度-广度优先
+	var bp func(nodes []*TreeNode, depth int) int
+	bp = func(nodes []*TreeNode, depth int) int {
+		next := make([]*TreeNode, 0, len(nodes)*2)
+		for _, node := range nodes {
+			if node == nil {
+				continue
+			}
+			if node.Left == nil && node.Right == nil {
+				return depth
+			}
+			next = append(next, node.Left, node.Right)
+		}
+		return bp(next, depth+1)
+	}
+	if root == nil {
+		return 0
+	}
+	return bp([]*TreeNode{root}, 1)
+}
+
+// 左叶子之和
+// 计算给定二叉树的所有左叶子之和。
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/sum-of-left-leaves
+func sumOfLeftLeaves(root *TreeNode) int {
+	var result int
+	var getLeft func(node *TreeNode, isLeft bool)
+	getLeft = func(node *TreeNode, isLeft bool) {
+		if node == nil {
+			return
+		}
+		if node.Left == nil && node.Right == nil && isLeft {
+			result += node.Val
+			return
+		}
+		getLeft(node.Left, true)
+		getLeft(node.Right, false)
+	}
+	getLeft(root, false)
+	return result
+}
+
+// 最长同值路径
+// 给定一个二叉树的root，返回最长的路径的长度 ，这个路径中的每个节点具有相同值。 这条路径可以经过也可以不经过根节点。
+//两个节点之间的路径长度由它们之间的边数表示。
+// [1,null,1,1,1,1,1,1] 的结果为4
+//          1
+//     nil     	  1
+//           1 			1
+//		1		1	1
+// [1,2,2,2,2,2,2,2]  的结果为3
+//			1
+//		2		2
+// 	 2	 2	  2	  2
+// 2
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/longest-univalue-path
+func longestUnivaluePath(root *TreeNode) int {
+	// 只允许存在一个根节点，而只有根节点允许子节点“分叉”，也就是只能“分叉”一次
+	// 每个节点都可能是根节点，因此需要保留其分叉的结果和未分叉的结果，供父节点进行比较、筛选
+	var result1 int
+	var longPath func(node *TreeNode) [2]int
+	longPath = func(node *TreeNode) [2]int {
+		if node == nil {
+			return [2]int{}
+		}
+		maxVal1, maxVal2 := 0, 0
+		left := longPath(node.Left)
+		right := longPath(node.Right)
+		if node.Left != nil && node.Right != nil && node.Left.Val == node.Val && node.Right.Val == node.Val {
+			maxVal2 = left[0] + right[0] + 2
+		}
+		maxVal2 = max(maxVal2, max(left[1], right[1]))
+		if node.Left != nil && node.Left.Val == node.Val {
+			maxVal1 = left[0] + 1
+		}
+		if node.Right != nil && node.Right.Val == node.Val {
+			maxVal1 = max(right[0]+1, maxVal1)
+		}
+		result1 = max(result1, maxVal1)
+		return [2]int{maxVal1, maxVal2}
+	}
+	result := longPath(root)
+	return max(result1, result[1])
+}
+
+func longestUnivaluePath2(root *TreeNode) int {
+	// 需要在递归过程中比较/生成最大值，递归过程中传递的值为当前节点作为单路径节点的最大长度
+	var result int
+	var dfs func(node *TreeNode, parentVal int) int
+	dfs = func(node *TreeNode, parentVal int) int {
+		if node == nil {
+			return 0
+		}
+		left := dfs(node.Left, node.Val)
+		right := dfs(node.Right, node.Val)
+		result = max(result, left+right)
+		if node.Val == parentVal {
+			return max(left, right) + 1
+		}
+		return 0
+	}
+	if root == nil {
+		return 0
+	}
+	dfs(root, root.Val)
+	return result
+}
+
+// 打家劫舍3
+// 小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为root。
+//除了root之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。
+//如果 两个直接相连的房子在同一天晚上被打劫 ，房屋将自动报警。
+//给定二叉树的root。返回在不触动警报的情况下，小偷能够盗取的最高金额。
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/house-robber-iii
+func rob(root *TreeNode) int {
+	// 每个节点都可以选择偷还是不偷,其状态影响到父节点和子节点
+	var dfs func(node *TreeNode) [2]int
+	dfs = func(node *TreeNode) [2]int {
+		if node == nil {
+			return [2]int{}
+		}
+		left := dfs(node.Left)
+		right := dfs(node.Right)
+		// 选择偷这个节点，那么子节点一定不能偷
+		steal := node.Val + left[0] + right[0]
+		// 如果不偷这个节点，那么子节点可以偷也可以不偷，选择最大值
+		noSteal := max(left[0], left[1]) + max(right[0], right[1])
+		return [2]int{noSteal, steal}
+	}
+	result := dfs(root)
+	return max(result[0], result[1])
+}
+
+// 二叉树中第二小的节点
+// 给定一个非空特殊的二叉树，每个节点都是正数，并且每个节点的子节点数量只能为2或0。
+// 如果一个节点有两个子节点的话，那么该节点的值等于两个子节点中较小的一个。
+// 更正式地说，即root.val = min(root.left.val, root.right.val) 总成立。
+// 给出这样的一个二叉树，你需要输出所有节点中的第二小的值 。
+//如果第二小的值不存在的话，输出 -1 。
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/second-minimum-node-in-a-binary-tree
+func findSecondMinimumValue(root *TreeNode) int {
+	// 根据题意，父节点一定是子节点中最小的那个，因此root节点就是最小值，只需要在其子孙节点中找到一个比它大的值即可
+	// 但是第二小的值不一定比第三小的值“高",因此不能用广度优先，而需要用深度优先
+	if root == nil {
+		return -1
+	}
+	rootVal := root.Val
+	result := -1
+	var dfs func(node *TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		if node.Val != rootVal && (result == -1 || node.Val < result) {
+			result = node.Val
+		}
+		dfs(node.Left)
+		dfs(node.Right)
+	}
+	dfs(root)
+	return result
+}
