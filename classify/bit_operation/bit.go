2dafa1314e82bbcbb2a9b69700d266613a6cd05b
stong <498327580@qq.com>
2022-02-18 10:45:30 +0800

    数组中唯一一个不重复的元素

--- classify/bit_operation/bit.go
+++ classify/bit_operation/bit.go
@@ -19,3 +19,16 @@ func hammingDistance(x int, y int) int {
 
 // 数组中唯一一个不重复的元素
 // https://leetcode-cn.com/problems/single-number/description/
+// 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+// 说明：
+// 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/single-number
+func singleNumber(nums []int) int {
+	// 计数可用哈希表，但是题目要求不使用额外的空间
+	// 在三个数a^b^c中，如果其中两个数相等，那么结果为另一个数
+	for i := 1; i < len(nums); i++ {
+		nums[i] ^= nums[i-1]
+	}
+	return nums[len(nums)-1]
+}
