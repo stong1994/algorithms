06506151934e0efb8918462c671cfc5f0e561fd0
stong <498327580@qq.com>
2022-03-03 11:28:38 +0800

    航班预订统计-差分数组

--- classify/pre_sum/pre_sum.go
+++ classify/pre_sum/pre_sum.go
@@ -106,8 +106,32 @@ func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
 	return this.sums[row2][col2] - this.sums[row1-1][col2] - this.sums[row2][col1-1] + this.sums[row1-1][col1-1]
 }
 
-// ["NumMatrix","sumRegion","sumRegion","sumRegion"]
-//[[[[-4,-5]]],
-// [0,0,0,0]
-// [0,0,0,1]
-// [0,1,0,1]]
+// 航班预订统计
+// 这里有n个航班，它们分别从 1 到 n 进行编号。
+// 有一份航班预订表bookings ，表中第i条预订记录bookings[i] = [firsti, lasti, seatsi]
+// 意味着在从 firsti到 lasti （包含 firsti 和 lasti ）的 每个航班 上预订了 seatsi个座位。
+// 请你返回一个长度为 n 的数组answer，里面的元素是每个航班预定的座位总数。
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/corporate-flight-bookings
+func corpFlightBookings(bookings [][]int, n int) []int {
+	result := make([]int, n)
+	//for _, booking := range bookings {
+	//	for i := booking[0]; i <= booking[1]; i++ {
+	//		result[i-1]+= booking[2]
+	//	}
+	//}
+	// 优化——差分数组（前缀和数组）：如[1,2,2,4]的差分数组为[1,1,0,2]，即第i个值为原数组第i个元素与第i-1个元素的差值
+	// 当对第2到3个元素加1时，只需要对差分数组中第二个元素加1，对第4个元素减1即可，即中间的元素不用修改
+	for _, booking := range bookings {
+		result[booking[0]-1] += booking[2]
+		if booking[1] < n {
+			result[booking[1]] -= booking[2]
+		}
+	}
+	for i := 1; i < n; i++ {
+		result[i] += result[i-1]
+	}
+	return result
+}
+
+// 拼车
