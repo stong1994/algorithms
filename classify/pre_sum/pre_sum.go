ce8672f6ba0cabb0b0b2f1cdae4f5a7fd9cdcbcf
stong <498327580@qq.com>
2022-03-03 11:46:09 +0800

    拼车-差分数组

--- classify/pre_sum/pre_sum.go
+++ classify/pre_sum/pre_sum.go
@@ -135,3 +135,42 @@ func corpFlightBookings(bookings [][]int, n int) []int {
 }
 
 // 拼车
+// 假设你是一位顺风车司机，车上最初有capacity个空座位可以用来载客。
+// 由于道路的限制，车只能向一个方向行驶（也就是说，不允许掉头或改变方向，你可以将其想象为一个向量）。
+// 这儿有一份乘客行程计划表trips[][]，其中trips[i] = [num_passengers, start_location, end_location]包含了第 i 组乘客的行程信息：
+// - 必须接送的乘客数量；
+// - 乘客的上车地点；
+// - 以及乘客的下车地点。
+// 这些给出的地点位置是从你的初始出发位置向前行驶到这些地点所需的距离（它们一定在你的行驶方向上）。
+// 请你根据给出的行程计划表和车子的座位数，来判断你的车是否可以顺利完成接送所有乘客的任务
+// （当且仅当你可以在所有给定的行程中接送所有乘客时，返回true，否则请返回 false）。
+// 提示：
+//	你可以假设乘客会自觉遵守 “先下后上” 的良好素质
+//	trips.length <= 1000
+//	trips[i].length == 3
+//	1 <= trips[i][0] <= 100
+//	0 <= trips[i][1] < trips[i][2] <= 1000
+//	1 <=capacity <= 100000
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/car-pooling
+func carPooling(trips [][]int, capacity int) bool {
+	// 差分数组
+	// trip的最大长度为1000，因此建立一个长度为1000的差分数组
+	// 遍历trips，对上车地点对应的trip增加当前人数，对下车地点对应的trip减去当前人数
+	// 最后遍历差分数组，看是否有超过capacity
+	list := make([]int, 1001)
+	for _, trip := range trips {
+		list[trip[1]] += trip[0]
+		list[trip[2]] -= trip[0]
+	}
+	if list[0] > capacity {
+		return false
+	}
+	for i := 1; i < 1001; i++ {
+		list[i] += list[i-1]
+		if list[i] > capacity {
+			return false
+		}
+	}
+	return true
+}
